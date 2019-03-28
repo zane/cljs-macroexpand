3d3bc17bd701157d28f566a82809143eed326615
Zane Shelby <zane@users.noreply.github.com>
2019-03-28 15:01:32 -0400

    WIP

--- src/cljs_macroexpand/core.cljs
+++ src/cljs_macroexpand/core.cljs
@@ -33,8 +33,7 @@
 
   (defmacro our-expand
     [form]
-    (println \"in macro\")
-    (fun/expand &env form))
+    (pr-str (fun/expand &env form)))
 ")
 
 (def state (cljs.js/empty-state))
@@ -43,7 +42,9 @@
   (cljs/load-analysis-cache! state 'cljs.analyzer (state/analyzer-state 'cljs.analyzer))
   nil)
 
-(load-library-analysis-cache!)
+(defn set-loaded!
+  [ns]
+  (swap! cljs/*loaded* conj ns))
 
 (defn deps
   [sym]
@@ -53,6 +54,17 @@
     'cljs-macroexpand.fun         fun-source
     'cljs-macroexpand.macro       macro-source))
 
+(def test-inc "(inc 0)")
+(def test-value "(ns cljs-macroexpand.test-value
+                   (:require [cljs-macroexpand.value :as value]))
+                 (inc value/my-value)")
+(def test-value-macro "(ns cljs-macroexpand.test-value-macro
+                         (:require-macros [cljs-macroexpand.value-macro :as value]))
+                       (println (value/value-macro))")
+(def test-macro "(ns cljs-macroexpand.test-macro
+                   (:require-macros [cljs-macroexpand.macro :as macro]))
+                 (println (macro/our-expand (let [x inc] (x 0))))")
+
 (defn load
   "https://cljs.github.io/api/cljs.js/STARload-fnSTAR"
   [{:keys [name macros path] :as opts} cb]
@@ -60,24 +72,21 @@
   (prn opts)
   (cb {:lang :clj, :source (deps name)}))
 
-(defn go
-  []
+
+#_(load-library-analysis-cache!)
+#_(set-loaded! 'cljs.analyzer)
+
+(defn eval-str
+  [s]
+
   (binding [cljs/*eval-fn* cljs/js-eval
             cljs/*load-fn* load]
     (cljs/eval-str #_(cljs/empty-state)
                    state
-                   #_
-                   "(inc 0)"
-                   #_
-                   "(ns cljs-macroexpand.test-0
-                      (:require [cljs-macroexpand.value :as value]))
-                    (inc value/my-value)"
-                   #_
-                   "(ns cljs-macroexpand.test-1
-                      (:require-macros [cljs-macroexpand.value-macro :as value]))
-                    (println (value/value-macro))"
-                   "(ns cljs-macroexpand.test-2
-                      (:require-macros [cljs-macroexpand.macro :as macro]))"
+                   s
                    println)))
 
-#_(go)
+(defn -main
+  [& _]
+  (set-loaded! 'cljs.analyzer)
+  (eval-str test-macro))
